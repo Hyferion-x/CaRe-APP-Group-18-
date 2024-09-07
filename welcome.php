05299ca468bccb8974d2657b8667d9a64812e3dc
Hyferion-x <utgw98@gmail.com>
2024-09-07 15:58:26 +0930

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,39 +1,3 @@
-<<<<<<< Updated upstream
-<?php
-session_start();
-
-// Check if the user is logged in
-if (!isset($_SESSION['username'])) {
-    header('Location: login.html'); // Redirect to login if not logged in
-    exit();
-}
-
-// Get user information from session
-$username = $_SESSION['username'];
-$name = $_SESSION['name'];
-$photo = $_SESSION['photo'];
-$role = $_SESSION['role'];
-
-// Redirect based on user role
-switch ($role) {
-    case 'patient':
-        header('Location: patient-dashboard.html');
-        exit();
-    case 'therapist':
-        header('Location: therapist-dashboard.html');
-        exit();
-    case 'staff':
-        header('Location: staff-dashboard.html');
-        exit();
-    case 'auditor':
-        header('Location: auditor-dashboard.html');
-        exit();
-    default:
-        echo "Invalid user role.";
-        exit();
-}
-?>
-=======
 <?php
 session_start();
 
